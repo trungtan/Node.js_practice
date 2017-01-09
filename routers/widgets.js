926a456141d396f028538e385992bb1ac2c220eb
Tan Bui the FIRMA <trungtanbui@gmail.com>
2017-01-09 10:00:32 +0200

    delete an item using DELETE

--- routers/widgets.js
+++ routers/widgets.js
@@ -4,39 +4,29 @@
 const express = require('express');
 const widgetRouter = express.Router();
 
+var data = [
+    { id: 1, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
+    { id: 2, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
+    { id: 3, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
+];
+
 widgetRouter.route('/widgets')
     .get((req, res) => {
-            res.json([
-                { id: 1, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
-                { id: 2, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
-                { id: 3, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
-            ]);
+            res.json(data);
         }
-    )
-    .post();
+    );
 
 widgetRouter.route('/widget/:widgetId')
     .get( (req, res) => {
-            //Assume a db connection with the ability to select data by ID
-            switch (parseInt(req.params.widgetId)) {
-                case 1:
-                    res.json([
-                        { id: 1, name: 'Bui Tan', email: 'tan@gmail.com', age: 28}
-                    ]);
-                    break;
-                case 2:
-                    res.json([
-                        { id: 2, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29}
-                    ]);
-                    break;
-                case 3:
-                    res.json([
-                        { id: 3, name: 'Tan Bui', school: 'TUAS', gender: 'male'}
-                    ]);
-                    break;
-            }
+            res.json(data[req.params.widgetId - 1]);
         }
-
+    )
+    .delete( (req, res) => {
+            data.splice(req.params.widgetId - 1, 1);
+            res.json([
+                {result: 'successful'}
+            ])
+        }
     );
 
 module.exports = widgetRouter;
