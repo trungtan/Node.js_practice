b6d40e28abc33280bcda522f2d30ca10f3d61249
Tan Bui the FIRMA <trungtanbui@gmail.com>
2017-01-09 10:50:19 +0200

    Update item with PUT

--- routers/widgets.js
+++ routers/widgets.js
@@ -5,17 +5,25 @@ const express = require('express');
 const widgetRouter = express.Router();
 
 var data = [
-    { id: 1, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
-    { id: 2, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
-    { id: 3, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
+    { id: 0, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
+    { id: 1, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
+    { id: 2, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
 ];
 
 widgetRouter.route('/widgets')
+    //GET /api/widgets HTTP/1.1
     .get((req, res) => {
             res.json(data);
         }
     )
-    .post( (req, res) => {
+    /**
+     * POST /api/widgets HTTP/1.1
+     Host: localhost:8080
+     Content-Type: application/x-www-form-urlencoded
+     Cache-Control: no-cache
+     id=10&name=From+Client&age=Brand+new
+     */
+     .post( (req, res) => {              //Content-type must be Content-Type: application/x-www-form-urlencoded
         let insertingItem = req.body;
         data.push(insertingItem);
         res.json([
@@ -25,18 +33,41 @@ widgetRouter.route('/widgets')
     });
 
 widgetRouter.route('/widget/:widgetId')
+    //GET /api/widget/2 HTTP/1.1
     .get( (req, res) => {
-            res.json(data[req.params.widgetId - 1]);
+            res.json(data[req.params.widgetId]);
         }
     )
+    //DELETE /api/widget/2
     .delete( (req, res) => {
-            let deletingItem = data[req.params.widgetId - 1];
-            data.splice(req.params.widgetId - 1, 1);
+            let deletingItem = data[req.params.widgetId];
+            data.splice(req.params.widgetId, 1);
             res.json([
                 {result: 'successful'},
                 {deleted_item: deletingItem}
             ])
         }
-    );
+    )
+
+    /**
+     PUT /api/widget/2 HTTP/1.1
+     Host: localhost:8080
+     Content-Type: application/x-www-form-urlencoded
+     Cache-Control: no-cache
+
+     name=edit+From+Client&age=Edit+Brand+new&newProp=Value+new+prop
+     */
+    .put( (req, res) => {
+        let updatingObj = JSON.parse(JSON.stringify(req.body));
+        updatingObj.id = req.params.widgetId;
+
+        data.splice(updatingObj.id, 1);
+        data.push(updatingObj);
+
+        res.json([
+            {result: 'successful'},
+            {updated_item: updatingObj}
+        ])
+    });
 
 module.exports = widgetRouter;
