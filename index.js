6a4753c3b0828fd88d8476eb9bd33e667fa3fcd2
Tan Bui the FIRMA <trungtanbui@gmail.com>
2017-01-09 09:12:13 +0200

    simplest demo

--- index.js
+++ index.js
@@ -16,12 +16,22 @@ const fs = require('fs');
 //const configFile = fs.readFileSync('./config.json');
 
 fs.readFile('./config.json', function (err, data) {
+    //1. Init and config
     const config = JSON.parse(data);
-
     const app = express();
     app.use(express.static(config.webServer.dir));
-
     const httpServer = http.createServer(app);
+
+    //2. Data, route and process
+    app.get('/api/widgets', (req, res) => {
+        res.json([
+            { name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
+            { name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
+            { name: 'Tan Bui', school: 'TUAS', gender: 'male'},
+        ]);
+    });
+
+    //3. Listening
     const port = process.env.PORT || config.webServer.port;
     httpServer.listen(port, function (err) {
         if (err) {
