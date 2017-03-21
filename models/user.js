62bf037ced27cd7cc545ffd46881b547ea9c6e9f
Tan Bui at home <tan.bui.home@gmail.com>
2017-03-21 22:49:27 +0200

    insert mock documents if the collection is empty

--- models/user.js
+++ models/user.js
@@ -10,16 +10,23 @@ const url = 'mongodb://localhost:27017/nodejs_practice';
 
 function insertMockDocuments() {
     MongoClient.connect(url, (err, db) => {
-        db.collection("users", (err, col) => {
-            col.insertMany([
-                { id: 0, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
-                { id: 1, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
-                { id: 2, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
-            ], (err, r) => {
-                assert.equal(null, err);
-                assert.equal(3, r.insertedCount)
-            });
+
+        db.collection('users').count((err, count) => {
+            if  (count == 0) {
+                db.collection("users", (err, col) => {
+                    col.insertMany([
+                        { id: 0, name: 'Bui Tan', email: 'tan@gmail.com', age: 28},
+                        { id: 1, name: 'Tan Bui', email: 'tanbui@gmail.com', age: 29},
+                        { id: 2, name: 'Tan Bui', school: 'TUAS', gender: 'male'},
+                    ], (err, r) => {
+                        assert.equal(null, err);
+                        assert.equal(3, r.insertedCount)
+                    });
+                });
+            }
         });
+
+
         db.close();
     });
 }
